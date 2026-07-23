0602abda23f5218f189c2336a2eb2d8a01b198f6
Dev <dev@Devs-MacBook-Pro.local>
2026-07-23 20:48:35 +0500

    Add Divide Functionality

---
+++ divide_numbers.py
@@ -0,0 +1,9 @@
+# Take input from the user
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+# Dividing the numbers
+division = num1 / num2
+
+# Display the result
+print("The result of division is: ", division)
