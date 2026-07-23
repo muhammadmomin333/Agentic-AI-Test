e143e9596b6b942574c66138bbc5313f2f512c74
Dev <dev@Devs-MacBook-Pro.local>
2026-07-23 21:49:04 +0500

    Subtraction Implemented

---
+++ subtract_numbers.py
@@ -0,0 +1,9 @@
+# Take input from the user
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+# Subtract the numbers
+subtraction = num1 + num2
+
+# Display the result
+print("The result of subtraction is: ", subtraction)
